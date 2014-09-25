56cf51ef805178fe9802a16c2ed3b326db9ec73c
Prateek Ojha <prateek479@gmail.com>
2014-09-25 19:59:51 +0530

    Update admin.js

--- public/routes/admin.js
+++ public/routes/admin.js
@@ -5,15 +5,6 @@ angular.module('mean.mean-admin').config(['$stateProvider', '$urlRouterProvider'
             .state('users', {
                 url: '/admin/users',
                 templateUrl: 'mean-admin/views/users.html'
-            }).state('themes', {
-                url: '/admin/themes',
-                templateUrl: 'mean-admin/views/themes.html'
-            }).state('settings', {
-                url: '/admin/settings',
-                templateUrl: 'mean-admin/views/settings.html'
-            }).state('modules', {
-                url: '/admin/modules',
-                templateUrl: 'mean-admin/views/modules.html'
             });
     }
 ]).config(['ngClipProvider',
